9fd807b503e76a02ac2289aea799abf7fc7f54ec
Kota Tsutsumi <kota3730@gmail.com>
2020-07-15 00:56:09 +0900

    create_ListAdd.vue
    ListAdd.vue作成

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,52 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const savedLists = localStorage.getItem('trello-lists')
+
+const store = new Vuex.Store({
   state: {
+    lists: savedLists ? JSON.parse(savedLists) : [
+      {
+        title: 'Backlog',
+        cards: [{
+          body: 'English',
+          memo: 'hahahah'
+          },
+          {
+            body: 'Mathematics',
+            memo: 'huuhuhuh'
+          },
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [{
+          body: 'Science',
+          memo: 'kakakaka'
+        }]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
   },
   mutations: {
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards: [] })
+    },
   },
   actions: {
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    },
   },
-  modules: {
+  getters: {
   }
 })
+
+store.subscribe((mutation, state) => {
+  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+
+export default store
